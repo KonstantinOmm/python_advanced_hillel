8fb17b53c049989b0062149177a961b0a3343876
Kostiantyn Omelianenko <konstantin230285@gmail.com>
2022-09-27 20:56:20 +0200

    django_hw_1

--- django_lms/groups/models.py
+++ django_lms/groups/models.py
@@ -1,3 +1,17 @@
-from django.db import models # noqa
+from datetime import date
+from django.core.validators import MinLengthValidator
+from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    group_name = models.CharField(
+        max_length=100,
+        verbose_name='group name',
+        db_column='group_name',
+        validators=[MinLengthValidator(2, '"group_name" field less than two symbols')]
+    )
+    group_start_date = models.DateField(default=date.today, null=True, blank=True)
+    group_description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.group_name
