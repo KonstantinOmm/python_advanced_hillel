a90f6717bc4bf34feaf38bc9366405ceb1502993
Kostiantyn Omelianenko <konstantin230285@gmail.com>
2022-10-02 11:42:59 +0200

    django_hw_2

--- django_lms/groups/models.py
+++ django_lms/groups/models.py
@@ -1,6 +1,9 @@
 from datetime import date
+
 from django.core.validators import MinLengthValidator
 from django.db import models
+
+from groups.validators import validate_start_date
 
 
 class Group(models.Model):
@@ -10,8 +13,11 @@ class Group(models.Model):
         db_column='group_name',
         validators=[MinLengthValidator(2, '"group_name" field less than two symbols')]
     )
-    group_start_date = models.DateField(default=date.today, null=True, blank=True)
+    group_start_date = models.DateField(default=date.today, null=True, blank=True, validators=[validate_start_date])
     group_description = models.TextField(max_length=1000)
 
     def __str__(self):
         return self.group_name
+
+    class Meta:
+        db_table = 'groups'
