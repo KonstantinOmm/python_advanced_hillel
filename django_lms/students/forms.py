81e55778f36b0fe38efe9c0fc539e71e2477971f
Kostiantyn Omelianenko <konstantin230285@gmail.com>
2022-10-07 16:55:15 +0200

    django_les_5

--- django_lms/students/forms.py
+++ django_lms/students/forms.py
@@ -14,6 +14,9 @@ class CreateStudentForm(forms.ModelForm):
             'email',
             'phone_number',
         ]
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
 
     def clean(self):
         pass
@@ -32,3 +35,17 @@ class CreateStudentForm(forms.ModelForm):
     def clean_phone_number(self):
         value = self.cleaned_data['phone_number']
         return "".join(char for char in value if char in "0123456789-()")
+
+
+class UpdateStudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = [
+            # '__all__',
+            'first_name',
+            'last_name',
+            'birthday',
+        ]
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
